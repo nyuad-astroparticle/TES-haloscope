feab0c67150ed9dd53b1ce6912af71e2aea28447
F7East <tengiz.ibrayev@gmail.com>
2023-10-18 09:40:42 +0400

    get photon count

--- include/SensitiveDetector.hh
+++ include/SensitiveDetector.hh
@@ -10,10 +10,12 @@ class SensitiveDetector : public G4VSensitiveDetector
     public:
         SensitiveDetector(const G4String & name);
         ~SensitiveDetector();
-        G4int photonCount;
         G4bool ProcessHits(G4Step *, G4TouchableHistory *);
 
+        G4int GetPhotonCount(){return photonCount;}
+
     private:
+        G4int photonCount;
 };
 
 #endif
--- src/RunAction.cc
+++ src/RunAction.cc
@@ -11,5 +11,5 @@ void RunAction::BeginOfRunAction(const G4Run * run)
 
 void RunAction::EndOfRunAction(const G4Run * run)
 {
-    G4cout << "Total photon count: " << aDetector->SD->photonCount;
+    G4cout << "Total photon count: " << aDetector->SD->GetPhotonCount();
 }
